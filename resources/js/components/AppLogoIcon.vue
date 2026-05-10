a674c617cc3c261d7c16ffae49dece9ddc6510cf
Leigh <leighdinaya04@gmail.com>
2026-05-10 08:00:50 +0800

    feat: update landing page and app logo icon

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -15,15 +15,27 @@ defineProps<Props>();
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 40 42"
+        viewBox="0 0 24 24"
         :class="className"
         v-bind="$attrs"
+        fill="none"
     >
         <path
+            d="M5 3.75h9.25L19 8.5v11.75H5V3.75Z"
+            stroke="currentColor"
+            stroke-width="1.8"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M14 4v5h5M8 9h3.5M8 12.5h5"
+            stroke="currentColor"
+            stroke-width="1.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="m9 18 1.05-3.15 5.45-5.45a1.49 1.49 0 0 1 2.1 2.1l-5.45 5.45L9 18Z"
             fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M17.2 5.633 8.6.855 0 5.633v26.51l16.2 9 16.2-9v-8.442l7.6-4.223V9.856l-8.6-4.777-8.6 4.777V18.3l-5.6 3.111V5.633ZM38 18.301l-5.6 3.11v-6.157l5.6-3.11V18.3Zm-1.06-7.856-5.54 3.078-5.54-3.079 5.54-3.078 5.54 3.079ZM24.8 18.3v-6.157l5.6 3.111v6.158L24.8 18.3Zm-1 1.732 5.54 3.078-13.14 7.302-5.54-3.078 13.14-7.3v-.002Zm-16.2 7.89 7.6 4.222V38.3L2 30.966V7.92l5.6 3.111v16.892ZM8.6 9.3 3.06 6.222 8.6 3.143l5.54 3.08L8.6 9.3Zm21.8 15.51-13.2 7.334V38.3l13.2-7.334v-6.156ZM9.6 11.034l5.6-3.11v14.6l-5.6 3.11v-14.6Z"
         />
     </svg>
 </template>
